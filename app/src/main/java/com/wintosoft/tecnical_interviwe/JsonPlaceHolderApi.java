681ffa32058edcee6a796c624aa2a6c201f98666
Avishka <avishka.asath@gmail.com>
2021-01-20 16:06:18 +0530

    Done

---
+++ app/src/main/java/com/wintosoft/tecnical_interviwe/JsonPlaceHolderApi.java
@@ -0,0 +1,19 @@
+package com.wintosoft.tecnical_interviwe;
+
+import com.wintosoft.tecnical_interviwe.Models.LiceneDetails;
+import com.wintosoft.tecnical_interviwe.Models.UserDetails;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface JsonPlaceHolderApi {
+
+    @GET("2")
+    Call<UserDetails> getUserDetails();
+
+
+
+}
